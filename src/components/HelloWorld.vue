ea8248d9b9b38186e7e99239755a8af29a921b84
aso2001054 <2001054@s.asojuku.ac.jp>
2020-07-22 07:33:51 +0000

    First Commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+    <p>{{ message }}</p>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+div {
+  margin: 0px;
+  padding: 0px;
+  text-align: left;
+}
+h1 {
+  font-size: 72pt;
+  font-weight: bold;
+  text-align: right;
+  letter-spacing: -8px;
+  color: #f0f0f0;
+  margin: 0px;
+}
+p {
+  margin: 0px;
+  color: #666;
+  font-size: 16pt;
+}
+</style>
